0fdf8be06a30103310beec5ae025e3105d8cf5d8
Vadym Vynnychuk <vadimvinnicuk4@gmail.com>
2020-04-01 17:56:48 +0300

    master

---
+++ js/js.js
@@ -0,0 +1,47 @@
+//1
+//Написать функцию, которая принимает 2 числа и возвращает -1, если первое меньше,
+// чем второе; 1 – если первое больше, чем второе; и 0 – если числа равны.
+
+
+// let number1 = prompt('Введіть перше число');
+// let number2 = prompt('Введіть друге число');
+// function numbers(x , y) {
+//     if (x < y) {
+//         return '-1'
+//     }else if (x > y) {
+//         return '1'
+//     }else if (x == y) {
+//         return '0'
+//     }
+
+// }
+// let result = numbers(number1 , number2);
+// alert(result);
+
+
+
+//2
+//Написать функцию, которая вычисляет факториал переданного ей числа.
+
+// let number = prompt('Введіть  число')
+// function factorial(n) {
+//     return (n != 1) ? n * factorial(n - 1) : 1;
+// }
+// alert( factorial(number) );
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
